c7bd7f0fe07caf133ca6af45186a3beb3f1f4bac
R-thinking <Making.R.W@gmail.com>
2021-04-11 15:12:53 +0900

    "JavaScript Commit_02" description: added a fuction to toggle subscribe button.

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+'use strict';
+
 const moreBtn = document.querySelector('.videoInfo .metadata .title .moreBtn');
-const title =document.querySelector('.videoInfo .metadata .title span');
+const title = document.querySelector('.videoInfo .metadata .title span');
+const subscribe = document.querySelector('.channel .subscribe');
 
 moreBtn.addEventListener('click',() =>{
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
 });
+
+
+subscribe.addEventListener('click',() =>{
+    let result = subscribe.classList.toggle('subscribed');
+    if(result) {
+        subscribe.textContent = "subscribed";
+    }
+    else {
+        subscribe.textContent = "subscribe";
+    }
+});
+
